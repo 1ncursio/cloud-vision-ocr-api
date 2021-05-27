b90526d1655eb54bc6ca4c9da3b88e519f1298a0
1ncursio <ckswn1323@naver.com>
2021-05-27 20:48:57 +0900

    Add shuffling array function

--- server/routes/words.js
+++ server/routes/words.js
@@ -12,6 +12,26 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // 무작위 인덱스(0 이상 i 미만)
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+router.get('/random', async (req, res, next) => {
+  try {
+    const words = await Word.findAll();
+    shuffle(words);
+
+    res.status(200).json(words);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 router.post('/', async (req, res, next) => {
   try {
     // const { hiragana, katakana, okurigana, kanji, korean, level } = req.body;
